385fd1d83973d9f944863f872a9b858daa3e2bda
rezptrri <aprilputra0308@gmail.com>
2026-08-15 14:41:24 +0800

    Update spinner.js

--- spinner.js
+++ spinner.js
@@ -47,7 +47,7 @@
           <div class="sp-wheel-center"></div>
         </div>
         <p class="sp-result-label">Giliran</p>
-        <p class="sp-result-name" id="sp-name">nungguin yeak?</p>
+        <p class="sp-result-name" id="sp-name">siapa yeahh?</p>
         <button class="sp-btn" id="sp-spin">Putar</button>
       </div>
     `;
